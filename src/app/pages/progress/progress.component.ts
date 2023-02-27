bf033cc01d9cdd40f5f61472aab654911761a31b
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2023-02-27 14:53:05 -0500

    Se crea componente reutilizable

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,14 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: ['./progress.component.css'
   ]
 })
 export class ProgressComponent implements OnInit {
+
+  progress1:number=15;
+  progress2:number=35;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  get getProgress1(){
+   return `${this.progress1}%`;
+  }
+
+  get getProgress2(){
+    return `${this.progress2}%`;
+   }
+
+
+
+
+
 
 }
